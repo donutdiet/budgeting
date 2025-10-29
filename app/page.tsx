5ec53253a601879af7ac599b1dba79d6a17fbcd2
donutdiet <72674098+donutdiet@users.noreply.github.com>
2025-10-28 21:12:22 -0400

    Remove some tutorial components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
 import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
 export default function Home() {
@@ -15,23 +12,23 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              <Link href={"/"}>Budgeting</Link>
               <div className="flex items-center gap-2">
                 <DeployButton />
               </div>
             </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+            <AuthButton />
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+            <SignUpUserSteps />
           </main>
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
           <p>
             Powered by{" "}
             <a
@@ -41,6 +38,14 @@ export default function Home() {
               rel="noreferrer"
             >
               Supabase
+            </a>{" "}
+            | Created by{" "}
+            <a
+              href="https://www.linkedin.com/in/jerrywang04/"
+              target="_blank"
+              className="font-bold hover:underline"
+            >
+              Jerry Wang
             </a>
           </p>
           <ThemeSwitcher />
