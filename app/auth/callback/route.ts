58a3ab7e37df67243fe4cac6efbf72f05ccb1648
donutdiet <72674098+donutdiet@users.noreply.github.com>
2025-11-02 21:44:40 -0500

    Adjust routing

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,15 +4,17 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/";
+  const next = searchParams.get("next") ?? "/protected";
 
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`);
+    } else {
+      return NextResponse.redirect(`/auth/error?error=${error?.message}`);
     }
   }
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  return NextResponse.redirect(`/auth/error?error=No token hash or type`);
 }
